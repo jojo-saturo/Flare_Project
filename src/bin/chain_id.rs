b6551e490b56a72d6fe14e5c752b622d44070888
jojo-saturo <niyitripplejoejoejoe@gmail.com>
2025-07-10 10:51:15 +0100

    Flare-Net config

---
+++ src/bin/chain_id.rs
@@ -0,0 +1,11 @@
+use alloy::providers::{Provider, ProviderBuilder};
+use eyre::Result;
+
+#[tokio::main]
+async fn main()-> Result<()> {
+    let provider = 
+        ProviderBuilder:: new().on_http("https://coston2-api.flare.network/ext/C/rpc".parse()?);
+    let chain_id = provider.get_chain_id().await?;
+    println!("Chain ID: {chain_id}");
+        Ok(())
+}
