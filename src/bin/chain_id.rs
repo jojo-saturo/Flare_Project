725508ba41a0a2f9602bf10c515a212e3bd1cd2b
jojo-saturo <niyitripplejoejoejoe@gmail.com>
2025-07-10 11:20:45 +0100

    Warning fixed/connect_http & importing Url

--- src/bin/chain_id.rs
+++ src/bin/chain_id.rs
@@ -1,10 +1,11 @@
 use alloy::providers::{Provider, ProviderBuilder};
 use eyre::Result;
+use url::Url;
 
 #[tokio::main]
 async fn main()-> Result<()> {
     let provider = 
-        ProviderBuilder:: new().on_http("https://coston2-api.flare.network/ext/C/rpc".parse()?);
+        ProviderBuilder:: new().connect_http(Url::parse("https://coston2-api.flare.network/ext/C/rpc")?);
     let chain_id = provider.get_chain_id().await?;
     println!("Chain ID: {chain_id}");
         Ok(())
